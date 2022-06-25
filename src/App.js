7a25beca4ad041306322d4350fba286d99b7874b
philipp aubert <p.b.m.aubert@gmail.com>
2022-06-26 00:11:58 +0200

    Add useReducer with two cases for setStories and filterStories, now moving to an impossible state handling

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { List } from './Items.js';
 import { Label } from './Label.js';
 import { Searchbar } from './Searchbar.js';
 import { Headline } from './Headline.js';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 
 const useSemiPersistentState = (key, initialValue) =>
 {
 	const [value, setValue] = useState(localStorage.getItem('search')||initialValue);
 
-	useEffect(()=>{
+	useEffect(() => {
 		localStorage.setItem('search', value);
 	}, [value]);
 
@@ -18,32 +18,47 @@ const useSemiPersistentState = (key, initialValue) =>
 }
 
 const getAsyncStories = () =>  
-	new Promise(resolve => 
+	new Promise((resolve, reject) => setTimeout(reject, 2000))/* resolve => 
 		setTimeout(
-			() => resolve({ data: { stories: list }}), 
+			() => resolve({ stories: list }), 
 			1500
-	)
-);
+	) */
+
+
+const storiesReducer = (state, action) => 
+{
+	switch (action.type) 
+	{
+		case 'SET_STORIES': 
+			return action.payload;
+		case 'REMOVE_STORIES':
+			return state.filter(story => action.payload.objectID !== story.objectID);
+		default: 
+			throw new Error();
+	}
+}		
 
 export const App  = () => {
 
 	const [value, setValue] = useSemiPersistentState('search', '');
 
-	const [stories, setStories] = useState([]);
+	const [stories, dispatchStories] = useReducer(
+		storiesReducer, 
+		[]
+	);
 
 	const [isLoading, setIsLoading] = useState(false);
 
+	const [isError, setIsError] = useState(false);
+
 	useEffect (() => {
 		setIsLoading(true);
-		getAsyncStories().then(result => {setStories(result.data.stories);
-		setIsLoading(false)})
+		getAsyncStories().then(result => {dispatchStories({type: 'SET_STORIES', payload: result.stories});
+		setIsLoading(false)}).catch(() => setIsError(true));
 	}, [])
 
 	const handleRemoveStories = (item) => 
-	{	
-		const newStories = stories.filter(story => item.objectID !== story.objectID) 
-		setStories(newStories);
-	}
+		dispatchStories({type: 'REMOVE_STORIES', payload: item});
 	
 	const onHandleInput  = (event)  => setValue(event.target.value);
 
@@ -56,6 +71,7 @@ export const App  = () => {
 			<Label title="type something"/>
 		</Searchbar>
 		<div className="list" >
+			{isError && <p>Something went wrong</p>}
 			{isLoading ? (<p>Loading...</p>) : (<List onRemove={handleRemoveStories} list={searchedStories}/>)}
 		</div>
     </div>
