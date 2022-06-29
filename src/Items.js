5b44bbd7f4eed584b77a4a2e1f9133f18f36444b
philipp aubert <p.b.m.aubert@gmail.com>
2022-06-29 20:05:02 +0200

    Add old sorting-method

--- src/Items.js
+++ src/Items.js
@@ -1,5 +1,45 @@
 import { Item } from './Item.js';
+import { useState } from 'react';
+import { sortBy } from 'lodash';
 
-export const List = ({ list, onRemove }) =>
-	console.log('List: B') ||
-	list.map(item => <Item key={item.objectID} onRemove={onRemove} item={item}/>);
+export const List = ({ list, onRemove }) => {
+
+	const SORTS = {
+		NONE: list => list,
+		TITLE: list => sortBy(list, 'title'),
+		AUTHOR: list => sortBy(list, 'author'),
+		COMMENTS: list => sortBy(list, 'num_comments').reverse(),
+		POINTS: list => sortBy(list, 'points').reverse(),
+	};
+
+	const [sort, setSort] = useState('NONE');
+
+	const sortFunction = SORTS[sort];
+
+	const sortedList = sortFunction(list);
+
+	const handleSort = (sortKey) => 
+	{
+		setSort(sortKey);
+	}
+
+	return (
+	<div>
+    <div className="listItem" >
+		<span className="listItem_" style={{ width: '40%' }}>
+			<button className="button" onClick={() => handleSort('TITLE')}>Title</button>
+		</span>
+		<span className="listItem_" style={{ width: '30%' }}>
+			<button className="button" onClick={() => handleSort('AUTHOR')}>Author</button>
+		</span>
+		<span className="listItem_" style={{ width: '10%' }}>
+			<button className="button" onClick={() => handleSort('COMMENTS')}>Comments</button>
+		</span>
+		<span className="listItem_" style={{ width: '10%' }}>
+				<button className="button" onClick={() => handleSort('POINTS')}>Points</button>
+		</span>
+		<span >Actions</span>
+    </div>
+		{sortedList.map(item => (<Item key={item.objectID} onRemove={onRemove} item={item}/>))}
+	</div>);
+}
